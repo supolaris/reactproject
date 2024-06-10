fe4df26d7d4fe003a8c872558762291c7c387e5f
Muhammad Suleman <msulaman061@gmail.com>
2024-06-10 18:43:40 +0500

    basic code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const listComponents = [
+  {
+    name: "James",
+  },
+  {
+    name: "Peter",
+  },
+  {
+    name: "Elizabeth",
+  },
+];
 
 function App() {
+  const [updateName, setUpdateName] = useState("Hello Coding");
+  const [counterValue, setCounterValue] = useState(0);
+
+  const [enteredName, setEnteredName] = useState();
+
+  const onIncreasePressed = () => {
+    if (counterValue >= 10) {
+      alert("Great Shot!");
+      return counterValue;
+    } else {
+      setCounterValue(counterValue + 1);
+    }
+  };
+
+  const onDecreasePressed = () => {
+    if (counterValue <= 0) {
+      return counterValue;
+    } else {
+      setCounterValue(counterValue - 1);
+    }
+  };
+
+  const MyForm = () => {
+    return (
+      <form>
+        <label>
+          Enter Your Name:
+          <input
+            type="text"
+            value={enteredName}
+            onChange={(name) => setEnteredName(name.target.value)}
+          />
+        </label>
+      </form>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1 onClick={() => setUpdateName("Hello coding 2")}>Update Name</h1>
+      <p>{updateName}</p>
+
+      {listComponents.map((item, index) => (
+        <li
+          key={item.name}
+          onClick={() => console.log("Name: ", item.name, "Index: ", index)}
         >
-          Learn React
-        </a>
-      </header>
+          {item.name}
+        </li>
+      ))}
+      <div style={{ flexDirection: "row" }}>
+        <p
+          onClick={onIncreasePressed}
+          style={{
+            fontSize: 20,
+            backgroundColor: "red",
+            width: "20%",
+            textAlign: "center",
+            borderRadius: 20,
+            paddingTop: 10,
+            paddingBottom: 10,
+          }}
+        >
+          Increase Value
+        </p>
+        <h2 style={{ textAlign: "center", width: "20%" }}>{counterValue}</h2>
+        <p
+          onClick={onDecreasePressed}
+          style={{
+            fontSize: 20,
+            backgroundColor: "red",
+            width: "20%",
+            textAlign: "center",
+            borderRadius: 20,
+            paddingTop: 10,
+            paddingBottom: 10,
+          }}
+        >
+          Decrease Value
+        </p>
+      </div>
+      <div>
+        <MyForm />
+      </div>
+      <div>Your name is: {enteredName}</div>
     </div>
   );
 }
---
+++ src/Screens/About.js
@@ -0,0 +1,7 @@
+export function About() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
